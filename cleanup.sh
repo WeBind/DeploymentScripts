62a045572ab1601a8ff3efc9bcd5169b07d95923
Mickael Goubin <micky0304@gmail.com>
2016-01-24 17:24:09 +0100

    same

--- cleanup.sh
+++ cleanup.sh
@@ -6,7 +6,7 @@ GLASSFISH_ASADMIN="$GLASSFISH_INSTALL/bin/asadmin"
 DOMAIN_PATH="$GLASSFISH_INSTALL/glassfish/domains/domain1/applications/*"
 for cur in "$@"
 do
-  ${GLASSFISH_ASADMIN} undeploy $BASE$cur
+  ${GLASSFISH_ASADMIN} undeploy --user admin --passwordfile ~/.passowrd_file $BASE$cur
 done
 rm -rf $DOMAIN_PATH
 
