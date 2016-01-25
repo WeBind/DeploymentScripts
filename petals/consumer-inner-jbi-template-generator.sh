21b57d2b4a3742dcb2be57e27909f0b036567d1a
Mickael Goubin <micky0304@gmail.com>
2016-01-25 09:49:26 +0100

    Fix template generators based on real jbi

--- petals/consumer-inner-jbi-template-generator.sh
+++ petals/consumer-inner-jbi-template-generator.sh
@@ -6,15 +6,17 @@ PROVIDER_SOAP=provider-$1
 HOST=$2
 ADDRESS="example"
 SERVICE_SOAP=$NAME
-INTERFACE_SOAP=$PROVIDER_SOAP
+INTERFACE_SOAP="SoapProvider"$1
+PORT_SOAP=$INTERFACE_SOAP"Port"
+
 cat << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <jbi:jbi xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:$ADDRESS="http://$ADDRESS/" xmlns:cdk5="http://petals.ow2.org/components/extensions/version-5" xmlns:jbi="http://java.sun.com/xml/ns/jbi" xmlns:soap="http://petals.ow2.org/components/soap/version-4" version="1.0">
   <jbi:services binding-component="true">
-    <jbi:consumes interface-name="$ADDRESS:$INTERFACE_SOAP" service-name="$SERVICE_NAME">
+    <jbi:consumes interface-name="$ADDRESS:$INTERFACE_SOAP">
       <cdk5:timeout>30000</cdk5:timeout>
       <cdk5:mep xsi:nil="true"/>
-      <soap:service-name>$SERVICE_SOAP</soap:service-name>
+      <soap:service-name>$INTERFACE_SOAP</soap:service-name>
       <soap:mode>SOAP</soap:mode>
       <soap:enable-http-transport>true</soap:enable-http-transport>
       <soap:enable-jms-transport>false</soap:enable-jms-transport>
--- petals/producer-outer-jbi-template-generator.sh
+++ petals/producer-outer-jbi-template-generator.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 NAME=$1
-PROVIDER_NAME=provider-$NAME
+PRODUCER_NAME=producer-$1
+PROVIDER_SOAP=provider-$1
+
+HOST=$2
+ADDRESS="example"
+SERVICE_SOAP=$NAME
+INTERFACE_SOAP="SoapProvider"$1
+PORT_SOAP=$INTERFACE_SOAP"Port"
+
 cat << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <jbi:jbi version="1.0"
@@ -10,19 +18,19 @@ cat << EOF
 
 	<jbi:service-assembly>
 		<jbi:identification>
-			<jbi:name>sa-SOAP-$PROVIDER_NAME-provide</jbi:name>
+			<jbi:name>sa-SOAP-$PROVIDER_SOAP-provide</jbi:name>
 			<jbi:description></jbi:description>
 		</jbi:identification>
 
 		<!-- New service-unit -->
 		<jbi:service-unit>
 			<jbi:identification>
-				<jbi:name>su-SOAP-$PROVIDER_NAME-provide</jbi:name>
+				<jbi:name>su-SOAP-$PROVIDER_SOAP-provide</jbi:name>
 				<jbi:description></jbi:description>
 			</jbi:identification>
 
 			<jbi:target>
-				<jbi:artifacts-zip>su-SOAP-$PROVIDER_NAME-provide.zip</jbi:artifacts-zip>
+				<jbi:artifacts-zip>su-SOAP-$PROVIDER_SOAP-provide.zip</jbi:artifacts-zip>
 				<jbi:component-name>petals-bc-soap</jbi:component-name>
 			</jbi:target>
 		</jbi:service-unit>
