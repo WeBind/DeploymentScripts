af0afbdb74f17986dd158c3c1958b2f4c4ac3140
Mickael Goubin <micky0304@gmail.com>
2016-01-25 15:51:20 +0100

    update cleanup to remotely delete files

--- cleanup.sh
+++ cleanup.sh
@@ -4,11 +4,19 @@ BASE="producer-"
 GLASSFISH_INSTALL="/usr/local/glassfish4"
 GLASSFISH_ASADMIN="$GLASSFISH_INSTALL/bin/asadmin"
 DOMAIN_PATH="$GLASSFISH_INSTALL/glassfish/domains/domain1/applications/*"
+
+REMOTE_HOST="192.168.0.105"
+REMOTE_HOST_FOLDER="/root/petals-esb-enterprise-edition-5.0.0-SNAPSHOT/esb/petals-esb-default-zip-5.0.1-SNAPSHOT/data/installed"
+
 for cur in "$@"
 do
   ${GLASSFISH_ASADMIN} undeploy --user admin --passwordfile ~/.passowrd_file $BASE$cur
+  ssh root@$REMOTE_HOST 'rm -rf $REMOTE_HOST_FOLDER/sa-SOAP-$BASE$cur-*'
 done
 rm -rf $DOMAIN_PATH
+rm -rf /tmp/webind/*
+rm -rf /tmp/webind-petals/
 
-${GLASSFISH_ASADMIN} restart-domain
+
+*${GLASSFISH_ASADMIN} restart-domain
 
