65ff067190ae926cc7f114578c2376b5286171d9
Mickael Goubin <micky0304@gmail.com>
2016-01-25 10:44:21 +0100

    fix wsdl downloaded name

--- petals/main.sh
+++ petals/main.sh
@@ -20,7 +20,7 @@ mkdir $ROOT/META-INF
 
 #Creation du inner zip
 ${PRODUCER_INNER_JBI_TEMPLATE_GENERATOR} $NAME $HOST > $ROOT/META-INF/jbi.xml
-wget -O $ROOT/SoapProviderService.wsdl http://$HOST:8080/$PROD_NAME/$PROV_NAME?wsdl
+wget -O $ROOT/$PROV_NAME.wsdl http://$HOST:8080/$PROD_NAME/$PROV_NAME?wsdl
 wget -O $ROOT/1.xsd http://$HOST:8080/$PROD_NAME/$PROV_NAME??xsd=1
 
 #outer zip
